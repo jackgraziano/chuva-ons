2db9475165f58ee4997347c5cf9b6a3f689650fd
Jackson Graziano <jackgraziano@gmail.com>
2017-08-20 19:32:47 -0300

    limite diario

--- lib/calculo.rb
+++ lib/calculo.rb
@@ -9,7 +9,7 @@ def calcula_remocao_vies(nome_bacia, data)
             "09": "SET", "10": "OUT", "11": "NOV", "12": "DEZ" }
   mes = meses[data[4..5].to_sym]
 
-  # carimba vetor de chuva na bacia
+  # Calculo ETA
   bacia.sub_bacias_ETA.each do |sub_bacia|
     sub_bacia.coordenadas.each do |c|
       key = sprintf("%.2f",c[:lat]).rjust(6, " ") + sprintf("%.2f",c[:lon]).rjust(6, " ")
@@ -19,7 +19,6 @@ def calcula_remocao_vies(nome_bacia, data)
                                   c[:chuva_ETA][:D4] + c[:chuva_ETA][:D5] + c[:chuva_ETA][:D6] +
                                   c[:chuva_ETA][:D7] + c[:chuva_ETA][:D8] + c[:chuva_ETA][:D9] +
                                   c[:chuva_ETA][:D10] ).round(2)
-
       # aplicar equacao de remocao de vies
       if c[:chuva_ETA][:soma_10] < sub_bacia.parametros["limite_#{mes}".to_sym]
         # aplicar eq de remocao de vies
@@ -38,6 +37,28 @@ def calcula_remocao_vies(nome_bacia, data)
       # limite de 10 dias
       c[:Ptotpr_mod10dias_lim] = [ c[:Ptotpr_mod10dias], sub_bacia.parametros["lim_10_dias_#{mes}".to_sym] ].min
 
+      # alfa
+      c[:alfa] = (c[:Ptotpr_mod10dias_lim] / c[:Ptotpr_mod10dias]).round(2)
+
+      c[:chuva_ETA_ajust] = Hash.new(0)
+      c[:chuva_ETA_ajust_lim] = Hash.new(0)
+      lim_diario = sub_bacia.parametros["lim_diario_#{mes}".to_sym]
+      # serie de 10 dias ajustada
+      (1..10).each do |i|
+        c[:chuva_ETA_ajust]["D#{i}".to_sym] = c[:chuva_ETA]["D#{i}".to_sym] * c[:alfa]
+        c[:chuva_ETA_ajust_lim]["D#{i}".to_sym] = [c[:chuva_ETA_ajust]["D#{i}".to_sym], lim_diario].min
+      end
+      c[:chuva_ETA_ajust][:soma_10] = ( c[:chuva_ETA_ajust][:D1] + c[:chuva_ETA_ajust][:D2] +
+                                        c[:chuva_ETA_ajust][:D3] + c[:chuva_ETA_ajust][:D4] +
+                                        c[:chuva_ETA_ajust][:D5] + c[:chuva_ETA_ajust][:D6] +
+                                        c[:chuva_ETA_ajust][:D7] + c[:chuva_ETA_ajust][:D8] +
+                                        c[:chuva_ETA_ajust][:D9] + c[:chuva_ETA_ajust][:D10] ).round(2)
+
+      c[:chuva_ETA_ajust_lim][:soma_10] = ( c[:chuva_ETA_ajust_lim][:D1] + c[:chuva_ETA_ajust_lim][:D2] +
+                                            c[:chuva_ETA_ajust_lim][:D3] + c[:chuva_ETA_ajust_lim][:D4] +
+                                            c[:chuva_ETA_ajust_lim][:D5] + c[:chuva_ETA_ajust_lim][:D6] +
+                                            c[:chuva_ETA_ajust_lim][:D7] + c[:chuva_ETA_ajust_lim][:D8] +
+                                            c[:chuva_ETA_ajust_lim][:D9] + c[:chuva_ETA_ajust_lim][:D10] ).round(2)
     end
   end
 
